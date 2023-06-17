215cf340085f50480716caeb502543eefb1289ae
Michael Karnitzschky <skizz8800ultra@googlemail.com>
2023-06-17 23:25:10 +0200

    Initial Commit

---
+++ app.py
@@ -0,0 +1,48 @@
+import datetime
+import os
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient(os.getenv("MONGODB_URI"))
+    app.db = client.emotionstagebuch
+
+    entries = []
+
+    @app.route("/", methods=["GET", "POST"])
+    def home():
+
+        entries_with_date = [
+            (
+                entry["content_situation"],
+                entry["content_reaktion"],
+                entry["content_verhalten"],
+                entry["date"],
+                datetime.datetime.strptime(entry["date"], "%d.%m.%Y, %H:%M").strftime("%b, %d"),
+            )
+            for entry in app.db.emotionen.find({})
+        ]
+
+        return render_template("home.html", entries=entries_with_date)
+
+    @app.route("/neu/", methods=["GET", "POST"])
+    def neu():
+        if request.method == "POST":
+            entry_situation = request.form.get("content_situation")
+            entry_reaktion = request.form.get("content_reaktion")
+            entry_verhalten = request.form.get("content_verhalten")
+            formatted_date = datetime.datetime.now().strftime("%d.%m.%Y, %H:%M")
+            app.db.emotionen.insert_one({"content_situation": entry_situation, "content_reaktion": entry_reaktion,
+                                         "content_verhalten": entry_verhalten, "date": formatted_date})
+
+        return render_template("neu.html")
+
+    return app
+
+# if __name__ == '__main__':
+#    app.run()
